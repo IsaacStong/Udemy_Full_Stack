dfe4356a9b98dfcd7a5ad04ae146b46f38f12245
IsaacStong <isaacstong90@gmail.com>
2020-08-15 13:10:54 -0400

    Django
    Template tagging for relative urls

--- Django/project_two/app_two/views.py
+++ Django/project_two/app_two/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-from app_two.models import User
+from app_two.forms import NewUser
 
 # Create your views here.
 def index(request):
     return render(request, 'app_two/index.html')
 
 def users(request):
-    user_list = User.objects.order_by('first_name')
-    user_dict = {'users': user_list}
-    return render(request, 'app_two/users.html', context=user_dict)
+    form = NewUser()
+    if request.method == 'POST':
+        form = NewUser(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print('Error')
+
+    return render(request, 'app_two/users.html', {'form': form})
